c723a934b8d441f3eb70590d9509da59df782174
ginnac <ginnacampbell1990@gmail.com>
2019-08-27 22:39:52 -0600

    survey.js - having the  submit button working properly, adding front end js validation, and getting  survey display correct number of times

--- public/js/survey.js
+++ public/js/survey.js
@@ -5,7 +5,7 @@ var groupname = locationArr[2];
 //we need to display survey data times === number of eaters 
 var $eaterNumber = $("#eater-number");
 var numberEaters;
-var $buttonSave = $("buttonSave");
+var $buttonSave = $("#buttonSave");
 
 var answers = [];
 
@@ -25,62 +25,87 @@ var API = {
 //function where i will call the getEaterNumber callback function and then I will 
 
 API.getEaterNumber(groupname).then(function (data) {
-
-  console.log(data.number_eaters);
   numberEaters = data.number_eaters;
 
-  //function to redeem survey page
-  function displaySurvey() {
+  displaySurvey(numberEaters);
 
-    if (numberEaters > 0) {
+});
 
-      //when clicking submit button 
-      $buttonSave.on("click", function (event) {
-        event.preventDefault();
 
-        //get values from survey handlebars
-        //store in a variable object and push to answersarray 
-        var userAnswers = {
-          answ1: $("#q1").val(),
-          answ2: $("#q2").val(),
-          answ3: $("#q3").val(),
-        }
+
+
+ //function to redeem survey page
+ function displaySurvey(numberEaters) {
+
+  console.log("hola");
+
+  if (numberEaters > 0) {
+
+    //when clicking submit button, used off and on to prevent duplicates
+    $buttonSave.off().on("click", function (event) {
+    //$("body").on("click", $buttonSave, function () {
+      event.preventDefault();
+
+      console.log(numberEaters);
+
+      //get values from survey handlebars
+      //store in a variable object and push to answersarray 
+      var userAnswers = {
+        answ1: $("#q1").val(),
+        answ2: $("#q2").val(),
+        answ3: $("#q3").val(),
+      }
+
+      var q1 = $("#q1").val();
+      var q2 = $("#q2").val();
+      var q3 = $("#q3").val();
+
+      //alerting user to enter all values
+      if (q1 === "" || q2 === "" || q3 === "") {
+        alert("Please select a choice for all the questions!");
+      }
+
+      //if there is valid choices selected then we push
+      else{
 
         answers.push(userAnswers);
-
+        console.log(answers);
         //reduce number of eaters -1,
         numberEaters--;
-
+        console.log(numberEaters);
         //clear values from the forms;
-
         $("#q1").val("");
         $("#q2").val("");
         $("#q3").val("");
 
+      }
+      
+      //used to prevent reading empty values for new eater, still need this to happen when number of eaters  === 0 regardless if answers === ""
+      if (q1 !== "" && q2 !== "" && q3 !== "" || numberEaters === 0) {
+        //call this function again;
+        displaySurvey(numberEaters);
 
-      });
-
-
-    //call this function again;
-    displaySurvey();
-
-
-
-    }
-
-    else {
-
-      //do logic to get the restaurant 
-      //display the next html - restautants
+      }
 
 
-    }
+    });
 
   }
 
-  displaySurvey();
+  else {
 
-})
+    //do logic to get the restaurant 
+     
+    //display the next html - restautants
+
+    window.location.href = "/";
+
+    console.log("done");
+
+
+  }
+
+}
 
 
 
