7626f5f45d3fe8a4499c820f29cbc735f880f4da
ginnac <ginnacampbell1990@gmail.com>
2019-08-27 18:37:29 -0600

    changes to survey js

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,16 +1,20 @@
 
-var locationArr= location.pathname.split("/");
+var locationArr = location.pathname.split("/");
 var groupname = locationArr[2];
-    
+
 //we need to display survey data times === number of eaters 
-var $eaterNumber =$("#eater-number");
+var $eaterNumber = $("#eater-number");
 var numberEaters;
+var $buttonSave = $("buttonSave");
+
+var answers = [];
+
 
 console.log(groupname);
 
 var API = {
- //ajax to get the eater number from database - getEaterNumber
- getEaterNumber: function(groupName) {
+  //ajax to get the eater number from database - getEaterNumber
+  getEaterNumber: function (groupName) {
     return $.ajax({
       url: "/api/experiences/" + groupName,
       type: "GET"
@@ -20,40 +24,61 @@ var API = {
 
 //function where i will call the getEaterNumber callback function and then I will 
 
-API.getEaterNumber(groupname).then(function(data){
+API.getEaterNumber(groupname).then(function (data) {
 
-    console.log(data.number_eaters);
-    numberEaters = data.number_eaters;
+  console.log(data.number_eaters);
+  numberEaters = data.number_eaters;
 
-    //function to redeem survey page
-    function displaySurvey(){
+  //function to redeem survey page
+  function displaySurvey() {
 
-        if(numberEaters > 0){
+    if (numberEaters > 0) {
 
-        //when clicking submit button push 
-        //store in a variable array 
+      //when clicking submit button 
+      $buttonSave.on("click", function (event) {
+        event.preventDefault();
 
-
-            //reduce number of eaters -1, 
-        
-            //clear values from the forms
-
-            //call this function again
-
-
+        //get values from survey handlebars
+        //store in a variable object and push to answersarray 
+        var userAnswers = {
+          answ1: $("#q1").val(),
+          answ2: $("#q2").val(),
+          answ3: $("#q3").val(),
         }
 
-        else{
+        answers.push(userAnswers);
 
-            //do logic to get the restaurant 
-            //display the next html - restautants
+        //reduce number of eaters -1,
+        numberEaters--;
 
-    
-        }
+        //clear values from the forms;
+
+        $("#q1").val("");
+        $("#q2").val("");
+        $("#q3").val("");
+
+
+      });
+
+
+    //call this function again;
+    displaySurvey();
+
+
 
     }
 
-    displaySurvey();
+    else {
+
+      //do logic to get the restaurant 
+      //display the next html - restautants
+
+
+    }
+
+  }
+
+  displaySurvey();
 
 })
 
