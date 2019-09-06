aacf2e5760f6d147025083fee7f87171e5740e1e
Nathalie Solis-Maravilla <nathaliesolis-maravilla@nathalies-mbp.uconnect.utah.edu>
2019-09-05 19:19:58 -0600

    deleted password and api

--- public/js/survey.js
+++ public/js/survey.js
@@ -244,7 +244,7 @@ function displaySurvey(numberEaters, numEater, zipcode) {
     console.log(zipcode);
 
     $.ajax({
-      url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDM07rWKSpGs6eZ788blx8FNXQI9SoxsZE&components=postal_code:" + zipcode + "&sensor=false",
+      url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAtCZISv6xfi48x9WbfjCY-yIolj9lo6tk&components=postal_code:" + zipcode + "&sensor=false",
       method: "POST",
       success: function (data) {
         console.log(data);
