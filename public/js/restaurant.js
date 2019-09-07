6161f393820c5fc82ec98ad0c8a0957cce33a182
Katsu Tanabe <katsu1@live.com>
2019-09-06 22:37:29 -0600

    adding more or less to css

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -35,7 +35,7 @@ function loadScript(src,callback){
  }
  
 //team please create your own key with google maps API; message me if you need help with that!! :)
- loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCtmvtrNe0IyrgPWkvzyjNdlPoER99nKto&libraries=places&callback=initialize', 
+ loadScript('https://maps.googleapis.com/maps/api/js?key=&libraries=places&callback=initialize', 
  function(){log('google-loader has been loaded, but not the maps-API ');});
  
  var map;
